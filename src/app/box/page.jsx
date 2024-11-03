d43c25bfbc1b2f14885555b35ffce415d64447e8
dani <emotion6172@gmail.com>
2024-11-03 08:45:59 -0500

    first commit

---
+++ src/app/box/page.jsx
@@ -0,0 +1,17 @@
+// src/app/box/page.jsx
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+
+export default function BoxPage() {
+  return (
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <mesh>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color="orange" />
+      </mesh>
+      <OrbitControls />
+    </Canvas>
+  );
+}
