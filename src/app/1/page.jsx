7602430bea586f80dc5aee48a62152573e1ebc03
dani <emotion6172@gmail.com>
2024-11-03 10:41:21 -0500

    5

--- src/app/1/page.jsx
+++ src/app/1/page.jsx
@@ -1,14 +1,23 @@
 'use client';
+
 import { useRef } from 'react';
 
-const SimpleRef = () => {
-  const divRef = useRef(null);
+const InputFocus = () => {
+  const inputRef = useRef(null);
+
+  const handleFocus = () => {
+    inputRef.current.focus();
+  };
 
   return (
-    <div ref={divRef}>
-      Hello World
+    <div>
+      <input ref={inputRef} type="text" placeholder="입력하세요" />
+      <button onClick={handleFocus}>포커스</button>
     </div>
   );
 };
 
-export default SimpleRef;
+export default InputFocus;
+
+// 이 코드는 버튼 클릭시 input 요소에 포커스를 주는 예제입니다.
+// useRef를 사용하여 input 요소를 참조하고, 버튼 클릭시 focus() 메서드를 호출합니다.
---
+++ src/app/1/page_0.jsx
@@ -0,0 +1,14 @@
+'use client';
+import { useRef } from 'react';
+
+const SimpleRef = () => {
+  const divRef = useRef(null);
+
+  return (
+    <div ref={divRef}>
+      Hello World
+    </div>
+  );
+};
+
+export default SimpleRef;
